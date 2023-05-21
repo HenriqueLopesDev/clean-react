b6724f6d5d463b9e5dc5eeb47baffbf813fd3f41
Henrique Lopes <henriquebonfanti2018@gmail.com>
2023-05-21 16:37:26 -0300

    refactor: add index files with multiple imports

--- src/presentation/pages/login/login.tsx
+++ src/presentation/pages/login/login.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import Styles from './login-styles.scss'
-import LoginHeader from '@/presentation/components/login-header/login-header'
-import Footer from '@/presentation/components/footer/footer'
-import Input from '@/presentation/components/input/input'
-import FormStatus from '@/presentation/components/form-status/form-status'
+import {
+  LoginHeader,
+  Input,
+  FormStatus,
+  Footer,
+} from '@/presentation/components'
 
 const Login: React.FC = () => {
   return (
