8a31f9bbf6ef7f9225adea4335b8b2c26d8f12a0
Henrique Lopes <henriquebonfanti2018@gmail.com>
2023-06-18 14:49:52 -0300

    fix: fix test tag typo

--- src/presentation/components/input/input.tsx
+++ src/presentation/components/input/input.tsx
@@ -25,7 +25,7 @@ const Input: React.FC<Props> = (props: Props) => {
 
   return (
     <div className={Styles.inputWrap}>
-      <input {...props} data-testId={props.name} onChange={handleChange} />
+      <input {...props} data-testid={props.name} onChange={handleChange} />
       <span
         data-testid={`${props.name}-status`}
         title={getTitle()}
